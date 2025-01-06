4c6f909d1d583695a3126e2f31562451173dcb82
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2025-01-06 14:50:01 -0600

    Render the App component to the root element

--- src/main.jsx
+++ src/main.jsx
@@ -3,3 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App"
 
 // Render the App component to the root element
+const div = document.getElementById("root")
+const root = ReactDOM.createRoot("div")
+root.render(<App />);
