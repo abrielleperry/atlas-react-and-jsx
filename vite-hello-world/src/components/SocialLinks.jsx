8c39b0ebafa98f2cf1cf2552dc45f56807825cd9
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2025-01-08 12:03:01 -0600

    tag for link is a not link *palm to face* i knew that

--- vite-hello-world/src/components/SocialLinks.jsx
+++ vite-hello-world/src/components/SocialLinks.jsx
@@ -5,12 +5,12 @@ import Github from "../assets/github.svg"
 function SocialLinks() {
   return (
     <div className="social-links">
-      <link href="www.linkedin.com/in/abriellerperry">
+      <a href="www.linkedin.com/in/abriellerperry">
       <img src={LinkedIn} alt="linkedin" />
-      </link>
-      <link href="https://github.com/abrielleperry">
+      </a>
+      <a href="https://github.com/abrielleperry">
       <img src={Github} alt="github" />
-      </link>
+      </a>
     </div>
   )
 }
