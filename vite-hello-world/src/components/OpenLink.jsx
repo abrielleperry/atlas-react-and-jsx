8c71eae6c7fd3bbc452fcf687ef3aef6cfa97519
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2025-01-09 13:25:11 -0600

    add link prop and Link component to OpenLink func

--- vite-hello-world/src/components/OpenLink.jsx
+++ vite-hello-world/src/components/OpenLink.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Open from "../assets/open.svg"
 
-function OpenLink() {
+export function OpenLink(props) {
+  <Link to={link} target="_blank" >
 
+  </Link>
 }
 
 export default OpenLink;
