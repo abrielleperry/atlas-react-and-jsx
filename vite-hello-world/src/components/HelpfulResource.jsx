294f2cd0fca0525a104c77a7c56fab65c7446df3
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2025-01-10 12:55:25 -0600

    add label prob, Openlink, Copylink, and pass link prop to Open and Copy

--- vite-hello-world/src/components/HelpfulResource.jsx
+++ vite-hello-world/src/components/HelpfulResource.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import OpenLink from "./OpenLink";
+import CopyLink from "./CopyLink";
 
 function HelpfulResource({ link, label }) {
   return (
     <div className="helpful-resource">
-
+      {label}
+      <OpenLink.link />
+      <CopyLink.link />
     </div>
   );
 }
