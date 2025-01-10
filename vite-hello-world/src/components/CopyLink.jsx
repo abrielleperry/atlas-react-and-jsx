6219409aef49a6c953128fa0ff9bf844d747575a
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2025-01-10 12:49:27 -0600

    add handleCopy function that uses clipboard api, add onclick attr to img tag

--- vite-hello-world/src/components/CopyLink.jsx
+++ vite-hello-world/src/components/CopyLink.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import CopySVG from "../assets/copy.svg";
 
 function CopyLink({ link }) {
+  function handleCopy() {
+    navigator.clipboard.writeText(link);
+  };
   return (
-    <img src={CopySVG} alt="Copy link" className="copy" />
-  )
+    <img src={CopySVG} alt="Copy link" className="copy" onClick={handleCopy}
+    />
+  );
 }
 
 export default CopyLink;
