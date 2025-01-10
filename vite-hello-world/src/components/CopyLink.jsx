7192893889948df4d462d21694663763bd008702
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2025-01-10 12:45:06 -0600

    add img tag with src, alt, and className copy

--- vite-hello-world/src/components/CopyLink.jsx
+++ vite-hello-world/src/components/CopyLink.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import CopySVG from "../assets/copy.svg";
 
 function CopyLink({ link }) {
-
+  return (
+    <img src={CopySVG} alt="Copy link" className="copy" />
+  )
 }
 
 export default CopyLink;
