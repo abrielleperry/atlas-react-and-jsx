1b8f37c8a1e3facdaa16266ec7837e52658d5f44
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2025-01-10 17:33:02 -0600

    list items contains array of links with their value and label, key attr set to index to render list

--- vite-hello-world/src/components/Footer.jsx
+++ vite-hello-world/src/components/Footer.jsx
@@ -5,7 +5,13 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer>
-   
+      <ul>
+        {links.map((link, index) => (
+        <li key={index}>
+          <a href={link.link} target="_blank">{link.label}</a>
+        </li>
+        ))}
+      </ul>
       <p>© {currentYear} Atlas School</p>
     </footer>
   );
